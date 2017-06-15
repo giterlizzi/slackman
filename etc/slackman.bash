260c6a7e8097150c13f1c6c3c3a613846de2e7dd
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2017-06-15 22:32:44 +0200

    etc/slackman.bash: Updated SlackMan bash-completion script

--- etc/slackman.bash
+++ etc/slackman.bash
@@ -1,22 +1,97 @@
-# Bash Completion for SlackMan
+#!/bin/bash
+#
+# bash completion file for slackman commands
+#
+# This script provides completion of:
+#  - commands and their options
+#  - repo id
+#
+# To enable the completions either:
+#  - place this file in /etc/bash_completion.d
+#  or
+#  - copy this file to e.g. ~/.slackman-completion.sh and add the line
+#    below to your .bashrc after bash completion features are loaded
+#    . ~/.slackman-completion.sh
 
-_slackman_list_repos() {
 
-  local repos=$(slackman list repo | grep ":" | awk '{ print $1 }')
-  local repos_short=$(echo $repos | awk -F ":" '{ print $1 }' | sort -u)
+__slackman_previous_extglob_setting=$(shopt -p extglob)
+shopt -s extglob
 
-  echo "$repos_short $repos"
+
+__slackman_to_extglob() {
+
+  local string="$1"
+  local extglob=${string// /|}
+
+  echo "@($extglob)"
 
 }
 
-_slackman_complete_options() {
+
+__slackman_subcommands() {
+
+  local subcommands="$1"
+  local counter=$(($command_pos + 1))
+
+  while [ $counter -lt $cword ]; do
+
+    case "${words[$counter]}" in
+      $(__slackman_to_extglob "$subcommands"))
+        subcommand_pos=$counter
+        local subcommand=${words[$counter]}
+        local completions_func=_slackman_${command}_${subcommand//-/_}
+        declare -F $completions_func >/dev/null && $completions_func
+        return 0
+        ;;
+      esac
+      (( counter++ ))
+  done
+  return 1
+
+}
+
+
+__slackman_list_installed_packages() {
+  echo $(slackman list installed | grep ":" | awk '{ print $1 }')
+}
+
+
+__slackman_list_packages() {
+  echo $(slackman list packages | grep ":" | awk '{ print $1 }')
+}
+
+
+__slackman_list_no_installed_packages() {
+  echo $(slackman list packages --exclude-installed | grep ":" | awk '{ print $1 }')
+}
+
+
+__slackman_list_repos() {
+
+  local slackman_cmd="slackman repo list"
+
+  case "$1" in
+    enabled)
+      echo $($slackman_cmd | grep -i enabled | grep ":" | awk '{ print $1 }')
+      ;;
+    disabled)
+      echo $($slackman_cmd | grep -i disabled | grep ":" | awk '{ print $1 }')
+      ;;
+    *)
+      echo $($slackman_cmd | grep ":" | awk '{ print $1 }')
+  esac
+
+  return
+
+}
+
+
+__slackman_complete_options() {
 
   case "$2" in
 
     --repo)
-      local repos=$(slackman list repo | grep ":" | awk '{ print $1 }')
-      local repos_short=$(echo $repos | awk -F ":" '{ print $1 }' | sort -u)
-      COMPREPLY=( $(compgen -W "$repos_short $repos" -- ${cur}) )
+      __slackman_complete_repos "enabled"
       return 0
     ;;
 
@@ -29,75 +104,236 @@ _slackman_complete_options() {
 
 }
 
-_slackman() {
+__slackman_complete_repos() {
+  COMPREPLY=( $(compgen -W "$(__slackman_list_repos "$@")" -- "$cur") )
+  __ltrim_colon_completions "$cur"
+}
 
-  local cur prev opts
 
-  COMPREPLY=()
+_slackman_slackman() {
 
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  local prev="${COMP_WORDS[COMP_CWORD-1]}"
-  local command="${COMP_WORDS[COMP_CWORD-2]}"
+  local slackman_options slackman_commands
 
-  slackman_options="-h --help --man --version -c --config --root --repo --quiet
-                    --download-only --new-packages --obsolete-packages -x --exclude
-                    --show-files --no-priority --no-excludes -y --yes -n --no"
-
-  slackman_commands="update upgrade install reinstall check-update remove repo db
+  slackman_options="-h --help --man --version -c --config --root"
+  slackman_commands="update upgrade install reinstall remove repo db
                      changelog search file-search history config help clean list"
+
+  if [[ "$cur" == -* ]]; then
+    COMPREPLY=( $( compgen -W "$slackman_options" -- "$cur" ) )
+    return
+  fi
+
+  COMPREPLY=($(compgen -W "$slackman_commands" -- ${cur}))
+  return
+
+}
+
+
+_slackman_repo_disable() {
+  __slackman_complete_repos "enabled"
+}
+
+
+_slackman_repo_enable() {
+  __slackman_complete_repos "disabled"
+}
+
+
+_slackman_repo_info() {
+  __slackman_complete_repos
+}
+
+
+_slackman_repo() {
+
+  local subcommands="list info disable enable"
+  __slackman_subcommands "$subcommands" && return
+  COMPREPLY=( $( compgen -W "$subcommands" -- "$cur" ) )
+
+}
+
+
+_slackman_info() {
+
+  COMPREPLY=( $(compgen -W "$(__slackman_list_packages "$@")" -- "$cur") )
+  __ltrim_colon_completions "$cur"
+
+}
+
+
+_slackman_update() {
+
+  local subcommands="packages history changelog manifest gpg-key all"
+
+  if [[ "$cur" == -* ]]; then
+    COMPREPLY=( $( compgen -W "--repo" -- "$cur" ) )
+    return 0
+  fi
+
+  __slackman_complete_options "$cur" "$prev" && return
+  __slackman_subcommands "$subcommands" && return
+
+  COMPREPLY=( $( compgen -W "$subcommands" -- "$cur" ) )
+
+}
+
+
+_slackman_upgrade() {
+
+  local slackman_options="--repo --exclude --download-only --summary"
+
+  __slackman_complete_options "$cur" "$prev" && return
 
   if [[ "$cur" == -* ]]; then
     COMPREPLY=( $( compgen -W "$slackman_options" -- "$cur" ) )
     return 0
   fi
 
-  case "${prev}" in
+  COMPREPLY=( $(compgen -W "$(__slackman_list_installed_packages "$@")" -- "$cur") )
+  __ltrim_colon_completions "$cur"
 
-    list)
-      COMPREPLY=( $( compgen -W 'installed obsoletes packages repo orphan variables' -- "$cur" ) )
-      return 0
-    ;;
+}
 
-    db)
-      COMPREPLY=( $( compgen -W 'info optimize' -- "$cur" ) )
-      return 0
-    ;;
 
-    update)
-      COMPREPLY=( $( compgen -W 'history packages changelog manifest gpg-key all' -- "$cur" ) )
-      return 0
-    ;;
+_slackman_changelog() {
 
-    repo)
-      COMPREPLY=( $( compgen -W 'list info enable disable' -- "$cur" ) )
-      return 0
-    ;;
+  local slackman_options="--repo --limit"
 
-    clean)
-      COMPREPLY=( $( compgen -W 'cache metadata manifest all' -- "$cur" ) )
-      return 0
-    ;;
+  __slackman_complete_options "$cur" "$prev" && return
 
-    info|disable|enable)
+  if [[ "$cur" == -* ]]; then
+    COMPREPLY=( $( compgen -W "$slackman_options" -- "$cur" ) )
+    return 0
+  fi
 
-      if [[ "$command" == "repo" ]]; then
-        repos=$(_slackman_list_repos)
-        COMPREPLY=( $(compgen -W "$repos" -- ${cur}) )
-        return 0
-      fi
+}
 
-    ;;
 
-    *)
-    ;;
+_slackman_install() {
 
-  esac
+  local slackman_options="--repo --exclude --download-only"
 
-  _slackman_complete_options "$cur" "$prev" && return 0
+  __slackman_complete_options "$cur" "$prev" && return
 
-  COMPREPLY=($(compgen -W "${slackman_commands}" -- ${cur}))
+  if [[ "$cur" == -* ]]; then
+    COMPREPLY=( $( compgen -W "$slackman_options" -- "$cur" ) )
+    return 0
+  fi
+
+  COMPREPLY=( $(compgen -W "$(__slackman_list_no_installed_packages "$@")" -- "$cur") )
+  __ltrim_colon_completions "$cur"
+
+}
+
+
+_slackman_reinstall() {
+
+  local slackman_options="--repo --exclude --download-only"
+
+  __slackman_complete_options "$cur" "$prev" && return
+
+  if [[ "$cur" == -* ]]; then
+    COMPREPLY=( $( compgen -W "$slackman_options" -- "$cur" ) )
+    return 0
+  fi
+
+  COMPREPLY=( $(compgen -W "$(__slackman_list_installed_packages "$@")" -- "$cur") )
+  __ltrim_colon_completions "$cur"
+
+}
+
+
+_slackman_remove() {
+
+  local slackman_options="--obsolete-packages"
+
+  if [[ "$cur" == -* ]]; then
+    COMPREPLY=( $( compgen -W "$slackman_options" -- "$cur" ) )
+    return 0
+  fi
+
+}
+
+
+_slackman_db() {
+
+  local subcommands="info optimize"
+  __slackman_subcommands "$subcommands" && return
+  COMPREPLY=( $( compgen -W "$subcommands" -- "$cur" ) )
+
+}
+
+
+_slackman_help() {
+
+  local subcommands="list repo db update"
+  __slackman_subcommands "$subcommands" && return
+  COMPREPLY=( $( compgen -W "$subcommands" -- "$cur" ) )
+
+}
+
+
+_slackman_list() {
+
+  local slackman_options="--no-installed"
+
+  __slackman_complete_options "$cur" "$prev" && return
+
+  if [[ "$cur" == -* ]]; then
+    COMPREPLY=( $( compgen -W "$slackman_options" -- "$cur" ) )
+    return 0
+  fi
+
+  local subcommands="installed obsoletes packages repo orphan variables"
+  __slackman_subcommands "$subcommands" && return
+  COMPREPLY=( $( compgen -W "$subcommands" -- "$cur" ) )
+
+}
+
+
+_slackman_clean() {
+
+  local subcommands="cache metadata manifest all"
+  __slackman_subcommands "$subcommands" && return
+  COMPREPLY=( $( compgen -W "$subcommands" -- "$cur" ) )
+
+}
+
+
+_slackman() {
+
+  local previous_extglob_setting=$(shopt -p extglob)
+  shopt -s extglob
+
+  COMPREPLY=()
+
+  local cur prev words cword
+  _get_comp_words_by_ref -n : cur prev words cword
+
+  local counter=1
+  local command='slackman' command_pos=0 subcommand_pos
+
+  while [ $counter -lt $cword ]; do
+    case "${words[$counter]}" in
+      *)
+        command="${words[$counter]}"
+        command_pos=$counter
+        break
+        ;;
+    esac
+    (( counter++ ))
+  done
+
+  local completions_func=_slackman_${command//-/_}
+  declare -F $completions_func >/dev/null && $completions_func
+
+  eval "$previous_extglob_setting"
   return 0
 
 }
+
+
+eval "$__slackman_previous_extglob_setting"
+unset __slackman_previous_extglob_setting
 
 complete -F _slackman slackman
