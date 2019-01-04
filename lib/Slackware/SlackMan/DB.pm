ccade36ae3af0ab6562be9e9855258f93e068d64
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2019-01-04 22:36:11 +0100

    Updated copyright year

--- lib/Slackware/SlackMan/DB.pm
+++ lib/Slackware/SlackMan/DB.pm
@@ -527,7 +527,7 @@ L<https://github.com/LotarProject/slackman/wiki>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2016-2018 Giuseppe Di Terlizzi.
+Copyright 2016-2019 Giuseppe Di Terlizzi.
 
 This module is free software, you may distribute it under the same terms
 as Perl.
